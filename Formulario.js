33fecc3ae94a56d30c8414b3d9c1ae0831843da2
juanardila15 <96641054+juanardila15@users.noreply.github.com>
2023-03-16 10:29:07 -0500

    Add files via upload

---
+++ Formulario.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react'
+
+const Formulario = () => {
+ return (
+  <Fragment>
+   <h2>Crear Cita</h2>
+
+   <form>
+    <label>Nombre Mascota</label>
+    <input type='text' />
+   </form>
+  </Fragment>
+ )
+}
+
+export default Formulario
